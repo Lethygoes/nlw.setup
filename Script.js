5a4bf3e8ac7b1f398e6206cfa6d3108f7b6cc5f5
Leticia Gonçalves <109564092+Lethygoes@users.noreply.github.com>
2023-01-24 22:33:08 -0300

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,27 @@
+const form = document.querySelector("#form-habits") 
+ const nlwSetup = new NLWSetup(form) 
+ const button = document.querySelector("header button") 
+  
+ button.addEventListener("click", add) 
+ form.addEventListener("change", save) 
+  
+ function add() { 
+   const today = new Date().toLocaleDateString("pt-br").slice(0, -5) 
+   const dayExists = nlwSetup.dayExists(today) 
+  
+   if (dayExists) { 
+     alert("Dia já incluso 🔴") 
+     return 
+   } 
+  
+   alert("Adicionado com sucesso ✅") 
+   nlwSetup.addDay(today) 
+ } 
+  
+ function save() { 
+   localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data)) 
+ } 
+  
+ const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {} 
+ nlwSetup.setData(data) 
+ nlwSetup.load()
